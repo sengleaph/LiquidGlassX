52ec0b26d687e9c2c7fad1260a38187cf7647c11
seang sengleaph <sengleaphseang@gmail.com>
2026-07-13 01:54:48 +0700

    push library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.recyclerview)
-    implementation(project(":liquidglass-view"))
+    implementation("com.github.sengleaph:LiquidGlassX:1.2.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
